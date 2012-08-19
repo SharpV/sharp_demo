354bb23116b0d9ae3d1116f2c7cc4c5af6dd27f8
root <root@ce-51.(none)>
2012-08-19 18:06:58 +0800

    bugfix for css

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,5 @@
 class Profile < ActiveRecord::Base
     
-  accepts_nested_attributes_for :actor
-  
-  validates_presence_of :actor_id
-  
   validates_format_of :mobile, :phone, :fax,
                       :allow_nil  => true,
                       :with       => /(^$)|(((\((\+?)\d+\))?|(\+\d+)?)[ ]*-?(\d+[ ]*\-?[ ]*\d*)+$)/,
